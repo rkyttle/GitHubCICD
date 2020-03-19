25a09ae58be01500356f1bf33eff4f2eabae6ec2
Ralph Kyttle <ralph.kyttle@microsoft.com>
2020-03-19 12:47:07 -0400

    Initial commit

---
+++ dsc/DSCSourceFilesForBuild/SetWinSecuritySettings/SetWinSecuritySettings.ps1
@@ -0,0 +1,56 @@
+﻿Configuration SetWinSecuritySettings
+{
+	param(
+	     [Parameter(HelpMessage='Timestamp used solely as a mechanism to force ARM to redeploy DSC resources because the parameters have changed.')]
+          [string]$Timestamp
+	)
+	
+	Import-DSCResource -ModuleName 'PSDesiredStateConfiguration'
+	Import-DSCResource -ModuleName 'AuditPolicyDSC'
+	Import-DSCResource -ModuleName 'SecurityPolicyDSC'
+	Node localhost
+	{
+         Registry 'Registry(POL): HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\NoDriveTypeAutoRun'
+         {
+              ValueName = 'NoDriveTypeAutoRun'
+              ValueType = 'Dword'
+              Key = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer'
+              ValueData = 255
+
+         }
+
+         AuditPolicySubcategory 'Audit Logon (Success) - Inclusion'
+         {
+              Name = 'Logon'
+              Ensure = 'Present'
+              AuditFlag = 'Success'
+
+         }
+
+          AuditPolicySubcategory 'Audit Logon (Failure) - Inclusion'
+         {
+              Name = 'Logon'
+              Ensure = 'Present'
+              AuditFlag = 'Failure'
+
+         }
+
+         UserRightsAssignment 'UserRightsAssignment(INF): Back_up_files_and_directories'
+         {
+              Policy = 'Back_up_files_and_directories'
+              Force = $True
+              Identity = @('*S-1-5-32-544')
+
+         }
+
+         SecurityOption 'SecuritySetting(INF): EnableGuestAccount'
+         {
+              Accounts_Guest_account_status = 'Disabled'
+              Name = 'Accounts_Guest_account_status'
+
+         }
+
+
+
+	}
+}
